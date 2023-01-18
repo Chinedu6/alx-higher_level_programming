a571ee0cbb913f2974e1937c551523afddbad2d0
Chinedu6 <105590061+Chinedu6@users.noreply.github.com>
2023-01-18 11:44:55 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,4 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    i = len(sys.argv) - 1
-
-    if i == 0:
-        print("{} arguments.".format(i))
-    elif i == 1:
-        print("{} argument:".format(i))
-    else:
-        print("{} arguments:".format(i))
-
-    if i >= 1:
-        i = 0
-        for arg in sys.argv:#!/usr/bin/python3
 
 if __name__ == "__main__":
     """Print the number of and list of arguments."""
@@ -27,6 +13,3 @@ if __name__ == "__main__":
         print("{} arguments:".format(count))
     for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
-            if i != 0:
-                print("{}: {}".format(i, arg))
-            i += 1
