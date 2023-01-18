87482e6e0fc40091055636d559f8acc9e940ef9c
Chinedu6 <105590061+Chinedu6@users.noreply.github.com>
2023-01-18 08:50:13 +0100

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,24 @@
 #!/usr/bin/node
-
 const request = require('request');
-const url = process.argv[2];
-
-request(url, function (err, response, body) {
+const req = {
+  url: process.argv[2],
+  method: 'GET'
+};
+request(req, function (err, response, body) {
   if (err) {
     console.log(err);
-  } else if (response.statusCode === 200) {
-    const completed = {};
-    const tasks = JSON.parse(body);
-    for (const i in tasks) {
-      const task = tasks[i];
+  } else {
+    body = JSON.parse(body);
+    const s = {};
+    body.forEach(task => {
       if (task.completed === true) {
-        if (completed[task.userId] === undefined) {
-          completed[task.userId] = 1;
+        if (s[task.userId] === undefined) {
+          s[task.userId] = 1;
         } else {
-          completed[task.userId]++;
+          s[task.userId]++;
         }
       }
-    }
-    console.log(completed);
-  } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
+    });
+    console.log(s);
   }
 });
